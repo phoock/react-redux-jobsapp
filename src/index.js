2cf381e96ad4b0d0785dc91a5197c8e378f12416
phoock <29984051@qq.com>
2018-06-19 15:41:23 +0800

    begin

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { createStore ,applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-
-//导入组件
-import Auth from './auth.jsx'
-import Dashboard from './dashboard.jsx'
-//导入reducer
-
+import { Provider } from 'react-redux'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import reducers from './reducer'
-import { Provider } from 'react-redux'
+import './config'
 
 const reduxDevtools = window.devToolsExtension?window.devToolsExtension:()=>{}
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
   reduxDevtools()
 ))
-function render(){
-  return ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/login' component={Auth}></Route>
-          <Route path='/dashboard' component={Dashboard}></Route>
-          <Redirect to="/dashboard"></Redirect>
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-    , document.getElementById('root')
-  )
-}
 
-render()
-store.subscribe(render)
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <BrowserRouter></BrowserRouter>
+    </Provider>
+  ),
+  document.getElementById('root')
+)
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,4 @@
 //合并所有的reducer,并且返回
 import { combineReducers} from 'redux'
-import { counter } from './index.redux.js'
-import { person } from './auth.redux.js'
 
-export default combineReducers({counter,person})
+export default combineReducers({})
