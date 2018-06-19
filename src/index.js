adc89667ff936f43334a1a0339cfd70527cec810
phoock <29984051@qq.com>
2018-06-19 16:49:04 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import reducers from './reducer'
 import './config'
 
+//引入页面
+import Login from './container/login/login.jsx'
+import Register from './container/register/register.jsx'
+
+
 const reduxDevtools = window.devToolsExtension?window.devToolsExtension:()=>{}
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
@@ -17,7 +22,12 @@ const store = createStore(reducers, compose(
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/register' component={Register}></Route>
+        </Switch>
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('root')
